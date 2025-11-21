dffd96368bba5e47746d9d44c58fd70a27497baf
770122whrt <Wang_Hao_Yu04@outlook.com>
2025-11-21 20:07:52 +0800

    merge the conflict

--- app/api/auth/[...all]/sign-in.ts
+++ app/api/auth/[...all]/sign-in.ts
@@ -25,39 +25,9 @@ const { data, error } = await authClient.signIn.email({
     //callbacks
 })
 
-<<<<<<< HEAD
-
-
-await authClient.signIn.social({
-    /**
-     * The social provider ID
-     * @example "github", "google", "apple"
-     */
-    provider: "github",
-    /**
-     * A URL to redirect after the user authenticates with the provider
-     * @default "/"
-     */
-    callbackURL: "/dashboard", 
-    /**
-     * A URL to redirect if an error occurs during the sign in process
-     */
-    errorCallbackURL: "/error",
-    /**
-     * A URL to redirect if the user is newly registered
-     */
-    newUserCallbackURL: "/welcome",
-    /**
-     * disable the automatic redirect to the provider. 
-     * @default false
-     */
-    disableRedirect: true,
-});
-=======
 // use data and error as needed
 if (error) {
   console.error("fail to sign in:", error);
 } else {
   console.log("Sign in successful:", data);
 }
->>>>>>> c1de285f9d20cb790eb6f20d1109fbc45675668b
--- app/api/auth/[...all]/sign-up.ts
+++ app/api/auth/[...all]/sign-up.ts
@@ -1,26 +1,6 @@
-import { email } from "better-auth";
+//import { email } from "better-auth";
 import { authClient } from "../../../lib/auth-client";
 
-<<<<<<< HEAD
-const { data, error } = await authClient.signUp.email({
-        email: email,// user email address get from front
-        password:"password1234", // user password -> min 8 characters by default get from front
-        name: "John Doe", // user display name get from front
-        //image:"https://example.com/image.png", // User image URL (optional)
-        callbackURL: "/dashboard" // A URL to redirect to after the user verifies their email (optional)
-    }, {
-        onRequest: (ctx) => {
-            //show loading
-        },
-        onSuccess: (ctx) => {
-            //redirect to the dashboard or sign in page
-        },
-        onError: (ctx) => {
-            // display the error message
-            alert(ctx.error.message);
-        },
-});
-=======
 // User details for sign-up
 const email = "user@example.com";
 const password = "mypassword123";
@@ -58,4 +38,3 @@ try {
 } catch (err) {
   console.error("Unexpected error:", err);
 }
->>>>>>> c1de285f9d20cb790eb6f20d1109fbc45675668b
